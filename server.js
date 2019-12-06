4b9a0c04a0238a994524a2db62a2d5c95a2469a5
Raul Alrejandro Salas Texido <alejandrosalas0891@gmail.com>
2019-12-06 22:06:47 +0100

    app ready

---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const crypto = require("crypto");
+const path  = require('path');
+const mongodbConnectionString = 'mongodb+srv://admin:server@cluster0-0r1vk.mongodb.net/uploadFiles?retryWrites=true&w=majority';
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+//Getting connection instance
+const connection = mongoose.createConnection(mongodbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+let gridfs;
+
+connection.on('open', () => {
+    console.log("[DEBUG]", "Server has been connected to mongodbcloud");
+    gridfs = new mongoose.mongo.GridFSBucket(connection.db, {
+        bucketName: "uploads"
+    });
+});
+
+//create instance Storage Engine
+const storage = new GridFsStorage({
+    url: mongodbConnectionString,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString("hex") + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: "uploads"
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+app.get('/', (req, res, next) => {
+    gridfs.find().toArray((error, files) => {
+        if (!files || files.length === 0) {
+            res.render('index', { files: false });
+        } else {
+            files.map(file => {
+                if (
+                    file.contentType === 'image/jpeg' ||
+                    file.contentType === 'image/png'
+                ) {
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+            res.render('index', { files: files });
+        }
+    });
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.redirect('/');
+});
+
+
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+    const file = gridfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist"
+        });
+      }
+      gridfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+// @route GET /files/:filename
+// @desc  Display single file object
+app.get('/files/:filename', (req, res) => {
+    const file = gridfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist"
+        });
+      }
+      gridfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+// @route DELETE /files/:id
+// @desc  Delete file
+app.delete('/files/:id', (req, res) => {
+    gridfs.delete(new mongoose.Types.ObjectId(req.params.id), (err, data) => {
+        if (err) return res.status(404).json({ err: err.message });
+        res.redirect("/");
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running in port 3000")
+})
